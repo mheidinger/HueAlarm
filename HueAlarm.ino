860f37f0d39fb44c45667d15f338983c53c4c6d3
Max Heidinger <mail@max-heidinger.de>
2019-05-24 18:16:01 +0200

    Further development

--- HueAlarm.ino
+++ HueAlarm.ino
@@ -6,13 +6,11 @@
 
 #include "credentials.h"
 
-#define LIGHT_NUM 1
+#define LIGHT_GROUP_NUM 1
 #define LIGHT_DELAY 120000
-#define TIMEZONE 1
-#define SUMMERTIME true
 
-int relaisInput = 12;
-int relaisOutput = 13;
+#define RELAIS_INPUT D1
+#define RELAIS_OUTPUT D2
 
 WiFiClient client;
 HTTPClient httpClient;
@@ -20,16 +18,16 @@ DynamicJsonDocument jsonDoc(JSON_OBJECT_SIZE(11) + 300);
 ESPHue hue(client, HUE_API_KEY, HUE_IP, HUE_PORT);
 
 void setup() {
-  pinMode(relaisInput, INPUT);
-  pinMode(relaisOutput, OUTPUT);
-  digitalWrite(relaisOutput, HIGH);
+  pinMode(RELAIS_INPUT, INPUT);
+  pinMode(RELAIS_OUTPUT, OUTPUT);
+  digitalWrite(RELAIS_OUTPUT, HIGH);
   
   Serial.begin(115200);
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(WIFI_SSID);
 
-  //WiFi.config();
+  //WiFi.config(); Can be used for a static IP
   WiFi.begin(WIFI_SSID, WIFI_PASSWD);
   
   while (WiFi.status() != WL_CONNECTED) {
@@ -41,43 +39,61 @@ void setup() {
   Serial.println("WiFi connected");  
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+
+  httpClient.setReuse(true);
 }
 
 void loop() {
-  if(digitalRead(relaisInput) == HIGH) {
+  if(digitalRead(RELAIS_INPUT) == HIGH) {
+    Serial.println("RelaisInput is HIGH");
+    
     if (isTestAlarm()) {
+      Serial.println("Detected TestAlarm, continue loop in 5 seconds");
       delay(5000);
       return;
     }
-    
-    hue.setLight(LIGHT_NUM, hue.ON, 0, 100, 0);
 
+    Serial.print("No TestAlarm: Turn on light group ");
+    Serial.println(LIGHT_GROUP_NUM);
+    hue.setGroup(LIGHT_GROUP_NUM, hue.ON, 0, 254, 0);
+
+    Serial.println("Start wait");
     unsigned long startMillis = millis();
-    while (millis() - startMillis < LIGHT_DELAY);
+    while (millis() - startMillis < LIGHT_DELAY) yield();
+    Serial.println("Wait finished");
 
-    hue.setLightPower(LIGHT_NUM, hue.OFF);
+    Serial.print("Turn off light group");
+    Serial.println(LIGHT_GROUP_NUM);
+    hue.setGroupPower(LIGHT_GROUP_NUM, hue.OFF);
   }
 }
 
 #define NUM(off, mult) ((datetime[(off)] - '0') * (mult))
 
 bool isTestAlarm() {
+  Serial.println("Start check for test alarm");
   if (httpClient.begin(client, "http://worldtimeapi.org/api/timezone/Europe/Berlin")) {
     int httpCode = httpClient.GET();
-    if (httpCode > 0) {
+    Serial.printf("Response is %d\n", httpCode);
+    if (httpCode == 200) {
       String payload = httpClient.getString();
       DeserializationError error = deserializeJson(jsonDoc, payload);
       if (error) {
+        Serial.print("Error deserializing payload: ");
+        Serial.println(error.c_str());
         httpClient.end();
         return false;
       }
       
       int day_of_week = jsonDoc["day_of_week"];
       const char* datetime = jsonDoc["datetime"];
+      Serial.print("Read datetime: ");
+      Serial.println(datetime);
 
       int hour =   NUM(9, 10)   + NUM(10, 1);
       int minute = NUM(11, 10)  + NUM(12, 1);
 
+      Serial.printf("Day of week is %d, hour is %d, minute is %d\n", day_of_week, hour, minute);
       if (day_of_week == 6 && ((hour == 11 && minute >= 55) || (hour == 12 && minute <= 5))) {
         httpClient.end();
         return true;
@@ -85,6 +101,9 @@ bool isTestAlarm() {
         httpClient.end();
         return false;
       }
+    } else if (httpCode < 0) {
+      Serial.print("HTTP Client Error: ");
+      Serial.println(httpClient.errorToString(httpCode).c_str());
     }
   }
   httpClient.end();
--- README.md
+++ README.md
@@ -3,7 +3,7 @@ HueAlarm
 
 Simple arduino sketch for a Wemos D1 Mini (ESP8266).
 Monitors a pin attached though a relais to a static HIGH. If the pin switches to HIGH, a Hue light will be switched on for two minutes.
-Used to turn on a lamp when a alarm comes in from a Swissphone ALGRA Expert pager station.
+Used to turn on a lamp when an alarm comes in from a Swissphone ALGRA Expert pager station.
 
 Requires the following libraries:
 - ESP8266 platform
