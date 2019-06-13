746e27e66ec010ef3590e1924e0aab0a880ca2b9
Max Heidinger <mail@max-heidinger.de>
2019-06-13 16:55:50 +0200

    Use NTP instead of an api for getting the time, filter for day / night alarm to decide whether to trigger the lights

--- HueAlarm.ino
+++ HueAlarm.ino
@@ -1,29 +1,33 @@
 #include <WiFiClient.h>
 #include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
+#include <WiFiUdp.h>
+#include <NTPClient.h>
 #include <ESPHue.h>
-#include <ArduinoJson.h>
 
 #include "credentials.h"
 
 #define LIGHT_GROUP_NUM 1
 #define LIGHT_DELAY 120000
 
+#define UTC_OFFSET_SEC 3600
+
 #define RELAIS_INPUT D1
 #define RELAIS_OUTPUT D2
 
 WiFiClient client;
-HTTPClient httpClient;
-DynamicJsonDocument jsonDoc(JSON_OBJECT_SIZE(11) + 300);
+WiFiUDP ntpUDP;
+
+NTPClient timeClient(ntpUDP, "pool.ntp.org", UTC_OFFSET_SEC);
 ESPHue hue(client, HUE_API_KEY, HUE_IP, HUE_PORT);
 
 void setup() {
-  pinMode(RELAIS_INPUT, INPUT);
+  pinMode(RELAIS_INPUT, INPUT_PULLUP);
   pinMode(RELAIS_OUTPUT, OUTPUT);
-  digitalWrite(RELAIS_OUTPUT, HIGH);
+  digitalWrite(RELAIS_OUTPUT, LOW);
   
   Serial.begin(115200);
   Serial.println();
+  Serial.println("==============================");
   Serial.print("Connecting to ");
   Serial.println(WIFI_SSID);
 
@@ -39,16 +43,17 @@ void setup() {
   Serial.println("WiFi connected");  
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+  Serial.println("==============================");
 
-  httpClient.setReuse(true);
+  timeClient.begin();
 }
 
 void loop() {
-  if(digitalRead(RELAIS_INPUT) == HIGH) {
-    Serial.println("RelaisInput is HIGH");
+  if(digitalRead(RELAIS_INPUT) == LOW) {
+    Serial.println("RelaisInput is LOW => Relais triggered");
     
-    if (isTestAlarm()) {
-      Serial.println("Detected TestAlarm, continue loop in 5 seconds");
+    if (isDay()) {
+      Serial.println("Detected day alarm, continue loop in 5 seconds");
       delay(5000);
       return;
     }
@@ -62,50 +67,26 @@ void loop() {
     while (millis() - startMillis < LIGHT_DELAY) yield();
     Serial.println("Wait finished");
 
-    Serial.print("Turn off light group");
+    Serial.print("Turn off light group ");
     Serial.println(LIGHT_GROUP_NUM);
     hue.setGroupPower(LIGHT_GROUP_NUM, hue.OFF);
+
+    Serial.println("==============================");
   }
 }
 
 #define NUM(off, mult) ((datetime[(off)] - '0') * (mult))
 
-bool isTestAlarm() {
-  Serial.println("Start check for test alarm");
-  if (httpClient.begin(client, "http://worldtimeapi.org/api/timezone/Europe/Berlin")) {
-    int httpCode = httpClient.GET();
-    Serial.printf("Response is %d\n", httpCode);
-    if (httpCode == 200) {
-      String payload = httpClient.getString();
-      DeserializationError error = deserializeJson(jsonDoc, payload);
-      if (error) {
-        Serial.print("Error deserializing payload: ");
-        Serial.println(error.c_str());
-        httpClient.end();
-        return false;
-      }
-      
-      int day_of_week = jsonDoc["day_of_week"];
-      const char* datetime = jsonDoc["datetime"];
-      Serial.print("Read datetime: ");
-      Serial.println(datetime);
+bool isDay() {
+  Serial.println("Start check for day alarm");
 
-      int hour =   NUM(9, 10)   + NUM(10, 1);
-      int minute = NUM(11, 10)  + NUM(12, 1);
+  timeClient.update();
+  int hours = timeClient.getHours();
 
-      Serial.printf("Day of week is %d, hour is %d, minute is %d\n", day_of_week, hour, minute);
-      if (day_of_week == 6 && ((hour == 11 && minute >= 55) || (hour == 12 && minute <= 5))) {
-        httpClient.end();
-        return true;
-      } else {
-        httpClient.end();
-        return false;
-      }
-    } else if (httpCode < 0) {
-      Serial.print("HTTP Client Error: ");
-      Serial.println(httpClient.errorToString(httpCode).c_str());
-    }
+  Serial.printf("Current hour is %d\n", hours);
+  if (hours > 9 && hours < 17) {
+    return true;
   }
-  httpClient.end();
+  
   return false;
 }
--- README.md
+++ README.md
@@ -8,6 +8,10 @@ Used to turn on a lamp when an alarm comes in from a Swissphone ALGRA Expert pag
 Requires the following libraries:
 - ESP8266 platform (https://github.com/esp8266/Arduino)
 - ESPHue (https://github.com/circuitsforfun/ESP_Hue)
-- ArduinoJson (https://github.com/bblanchon/ArduinoJson)
+- NTPClient (https://github.com/arduino-libraries/NTPClient)
 
 The sketch needs to be configured with a `credentials.h` file that can be copied from `credentials-dummy.h` and filled with proper values.
+For the ESP that I am currently using (WEMOS D1 Mini), select the board `LOLIN(WEMOS) D1 R2 & mini`.
+
+How to find the IP address of the Hue Bridge: https://huetips.com/help/how-to-find-my-bridge-ip-address/
+How to get the Hue API key: https://developers.meethue.com/develop/hue-api/7-configuration-api/
